ba2d03c80057f188432f5254faadec29c602ae96
WalterBaeck <walter.baeck@gmail.com>
2026-01-03 00:10:54 +0100

    More organised tree search
    Same methodology as Advent2025_10_2.c but more tightly grouped into my favorite tree search function Descent (progress), Ascent (backtrack) and Increase (branch choice).
    Probably correct just like Advent2025_10_2.c but eternally slow.

---
+++ Advent2025_10_2c.c
@@ -0,0 +1,381 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int Arguments, char* Argument[])
+{
+
+  /******************/
+  /* Initialisation */
+  /******************/
+  FILE* InputFile = stdin;
+  if (Arguments >= 2)
+    if (!(InputFile = fopen(Argument[1], "r")))
+    {
+      fprintf(stderr, "Could not open %s for reading\n", Argument[1]);
+      exit(1);
+    }
+  int InputLineNr = 0;
+  char InputLine[1000];
+  char *InputStart, *InputEnd, *InputPtr;
+
+  char InputSegment[100];
+  int Num;
+
+  int LightNr, NrOfLights, OtherLightNr;
+  int ButtonNr, NrOfButtons, Button[30][30];
+  int Joltage[30] = {0};
+
+  int LightOccurs, MinLightOccurs, MinLightNr;
+  int Order, LightDone[30], LightOrder[30], ButtonDone[30];
+  int ButtonAtOrder[30][30], NrOfButtonAtOrder[30];
+
+  int Index, MaxAtOrder[30], Partition[30][30];
+
+  int Total[30], Push[30];
+  int FakeResult, NrOfPush, MinNrOfPush, Sum=0;
+
+  /*************/
+  /* Operation */
+  /*************/
+  while (fgets(InputLine, 1000, InputFile))
+  {
+    InputLineNr++;
+    while ((InputLine[strlen(InputLine) - 1] == '\r')
+        || (InputLine[strlen(InputLine) - 1] == '\n'))
+      InputLine[strlen(InputLine) - 1] = '\0';
+    //////////////////// Parse the lights ////////////////////
+    InputStart = InputLine;
+    if (*InputStart != '[')
+    {
+      fprintf(stderr, "Could not find [ in InputLine #%d:\n%s\n",
+          InputLineNr, InputLine);
+      exit(2);
+    }
+    if (!(InputEnd = strchr(InputStart, ']')))
+    {
+      fprintf(stderr, "Could not find [ in InputLine #%d:\n%s\n",
+          InputLineNr, InputLine);
+      exit(2);
+    }
+    NrOfLights = InputEnd - InputStart - 1;
+    if (NrOfLights > 30)
+    {
+      fprintf(stderr, "Cannot handle NrOfLights=%d on InputLine #%d\n",
+          NrOfLights, InputLineNr);
+      exit(3);
+    }
+    for (LightNr=0; LightNr<NrOfLights; LightNr++)
+      if ((InputStart[LightNr+1] != '#') && (InputStart[LightNr+1] != '.'))
+      {
+        fprintf(stderr, "Unknown Light Target #%d : %c on InputLine #%d\n",
+            LightNr, InputStart[LightNr+1], InputLineNr);
+        exit(2);
+      }
+    //////////////////// Parse the buttons ////////////////////
+    ButtonNr = 0;
+    for (;;)
+    {
+      if (InputEnd[1] != ' ')
+      {
+        fprintf(stderr, "Expecting a space instead of %c at pos %d of InputLine #%d\n",
+            InputEnd[1], InputEnd - InputLine + 1, InputLineNr);
+        exit(2);
+      }
+      if (InputEnd[2] != '(')  break;
+      InputStart = InputEnd + 3;
+      if (!(InputEnd = strchr(InputStart, ')')))
+      {
+        fprintf(stderr, "Could not find ) in InputLine #%d:\n%s\n",
+            InputLineNr, InputLine);
+        exit(2);
+      }
+      strncpy(InputSegment, InputStart, InputEnd - InputStart);
+      InputSegment[InputEnd - InputStart] = '\0';
+      memset(Button[ButtonNr], 0, 30 * sizeof(int));
+      for (Num=0; InputPtr = strtok(Num ? NULL : InputSegment, ","); Num++)
+      {
+        if (1 != sscanf(InputPtr, "%d", &LightNr))
+        {
+          fprintf(stderr, "Could not scan Button #%d Num #%d from %s on InputLine #%d\n",
+              ButtonNr, Num, InputPtr, InputLineNr);
+          exit(2);
+        }
+        Button[ButtonNr][LightNr] = 1;
+      } /* for (strtok) */
+      ButtonNr++;
+      if (ButtonNr >= 30)
+      {
+        fprintf(stderr, "Cannot handle NrOfButtons=%d on InputLine #%d\n",
+            ButtonNr+1, InputLineNr);
+        exit(3);
+      }
+    } /* forever */
+    NrOfButtons = ButtonNr;
+    //////////////////// Parse the joltages ////////////////////
+    if (InputEnd[2] != '{')
+    {
+      fprintf(stderr, "Expecting { instead of %c at pos %d of InputLine #%d\n",
+          InputEnd[1], InputEnd - InputLine + 2, InputLineNr);
+      exit(2);
+    }
+    InputStart = InputEnd + 3;
+    if (!(InputEnd = strchr(InputStart, '}')))
+    {
+      fprintf(stderr, "Could not find } in InputLine #%d:\n%s\n",
+          InputLineNr, InputLine);
+      exit(2);
+    }
+    strncpy(InputSegment, InputStart, InputEnd - InputStart);
+    InputSegment[InputEnd - InputStart] = '\0';
+    for (LightNr=0; InputPtr = strtok(LightNr ? NULL : InputSegment, ","); LightNr++)
+      if (1 != sscanf(InputPtr, "%d", &(Joltage[LightNr])))
+      {
+        fprintf(stderr, "Could not scan Joltage #%d from %s on InputLine #%d\n",
+            LightNr, InputPtr, InputLineNr);
+        exit(2);
+      }
+    if (LightNr != NrOfLights)
+    {
+      fprintf(stderr, "%d Joltages but %d Lights on InputLine #%d ?\n",
+          LightNr, NrOfLights, InputLineNr);
+      exit(2);
+    }
+
+/* Debug */
+if (1)  printf("Line #%d: Found %d Lights, %d Buttons\n", InputLineNr, NrOfLights, NrOfButtons);
+
+    //////////////////// Establish LightOrder ////////////////////
+    memset(LightOrder, 0, 30 * sizeof(int));
+    memset(LightDone, 0, 30 * sizeof(int));
+    memset(ButtonDone, 0, 30 * sizeof(int));
+    memset(NrOfButtonAtOrder, 0, 30 * sizeof(int));
+    for (Order=0; Order<NrOfLights; Order++)
+    {
+      MinLightOccurs = -1;
+      memset(ButtonAtOrder[Order], 0, 30 * sizeof(int));
+      // Go over all lights that can still be affected by unused buttons
+      for (LightNr=0; LightNr<NrOfLights; LightNr++)
+      {
+        if (LightDone[LightNr])  continue;
+        LightOccurs = 0;
+        for (ButtonNr=0; ButtonNr<NrOfButtons; ButtonNr++)
+        {
+          if (ButtonDone[ButtonNr])  continue;
+          if (Button[ButtonNr][LightNr])  LightOccurs++;
+        }
+        if (!LightOccurs)
+        {  LightDone[LightNr] = 1;  continue; }
+        if ((MinLightOccurs == -1) || (LightOccurs < MinLightOccurs))
+        {  MinLightOccurs = LightOccurs;  MinLightNr = LightNr; }
+      }
+      if (MinLightOccurs == -1)  break;
+      // So, this LightNr is affected by fewest remaining available buttons
+      LightOrder[Order] = MinLightNr;
+      LightDone[MinLightNr] = 1;
+/* Debug */
+if (1)  printf(" At Order #%d, settle Light #%d with Buttons", Order, MinLightNr);
+      for (ButtonNr=0; ButtonNr<NrOfButtons; ButtonNr++)
+        if ((Button[ButtonNr][MinLightNr]) && (!ButtonDone[ButtonNr]))
+        {
+          ButtonDone[ButtonNr] = 1;
+          ButtonAtOrder[Order][NrOfButtonAtOrder[Order]++] = ButtonNr;
+/* Debug */
+if (1)  printf(" %d", ButtonNr);
+        }
+/* Debug */
+if (1)  putchar('\n');
+    } /* for (Order) */
+
+    //////////////////// Debugging ////////////////////
+    void PrintDebug()
+    {
+      printf("+%3d+", Total[0]);
+      for (Num=1; Num<NrOfLights; Num++)  printf("%2d+", Total[Num]);
+      printf("  Push#%3d#", Push[0]);
+      for (Num=1; Num<NrOfButtons; Num++)  printf("%2d#", Push[Num]);
+      putchar('\n');
+      printf("-%3d-", Joltage[0] - Total[0]);
+      for (Num=1; Num<NrOfLights; Num++)  printf("%2d-", Joltage[Num] - Total[Num]);
+      printf("  P[%d]#%3d#", Order, Partition[Order][0]);
+      for (Num=1; Num<NrOfButtonAtOrder[Order]; Num++)  printf("%2d#", Partition[Order][Num]);
+      putchar('\n');
+    }
+
+
+    //////////////////// Priming ////////////////////
+    MinNrOfPush = -1;
+    memset(Total, 0, 30 * sizeof(int));
+    memset(Push, 0, 30 * sizeof(int));
+    Order = -1;
+
+    //////////////////// Examine all possible partitions ////////////////////
+    for (;;)
+    {
+      //////////////////// Descent ////////////////////
+      for (++Order; Order<NrOfLights; Order++)
+      {
+        // Reset every Partition at downstream Orders
+        if (!NrOfButtonAtOrder[Order])  break;  // End reached
+        LightNr = LightOrder[Order];
+        int Needed = Joltage[LightNr] - Total[LightNr];  // May become negative !
+        if (Needed < 0)
+        {
+/* Debug */
+if (1)  printf("  v Incomplete: Order #%d Light #%d wants Joltage %d but Total at %d\n",
+Order, LightNr, Joltage[LightNr], Total[LightNr]);
+PrintDebug();
+          break;
+        }
+        MaxAtOrder[Order] = Needed;
+        memset(Partition[Order], 0, 30 * sizeof(int));
+        Index = NrOfButtonAtOrder[Order] - 1;
+        Partition[Order][Index] = Needed;
+        ButtonNr = ButtonAtOrder[Order][Index];
+        Push[ButtonNr] = Needed;
+        for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+          if (Button[ButtonNr][OtherLightNr])
+            Total[OtherLightNr] += Needed;
+/* Debug */
+if (1)  printf("  v Order #%d Light #%d, setting Button=%d to Push=%d\n",
+Order, LightNr, ButtonNr, Needed);
+PrintDebug();
+      } /* for (Order) */
+
+      //////////////////// Result Check ////////////////////
+      if ((!NrOfButtonAtOrder[Order]) && (!memcmp(Total, Joltage, NrOfLights * sizeof(int))))
+      {
+        NrOfPush = 0;
+        for (ButtonNr=0; ButtonNr<NrOfButtons; ButtonNr++)
+          NrOfPush += Push[ButtonNr];
+        if ((MinNrOfPush == -1) || (NrOfPush < MinNrOfPush))
+        {
+          MinNrOfPush = NrOfPush;
+/* Debug */
+if (1)  printf("  - MinNrOfPush=%d\n", MinNrOfPush);
+        }
+      }
+
+      //////////////////// Ascent ////////////////////
+Ascent:
+      for (--Order; Order>=0; Order--)
+        if (NrOfButtonAtOrder[Order] == 1)
+        {
+          // No free choice here, need to backtrack further
+          // Bookkeeping for this abandoned Order
+          ButtonNr = ButtonAtOrder[Order][0];
+/* Debug */
+if (1)  printf("  ^ Trivial Order #%d resetting Button=%d from Push=%d\n",
+Order, ButtonNr, Push[ButtonNr]);
+          for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+            if (Button[ButtonNr][OtherLightNr])
+              Total[OtherLightNr] -= Push[ButtonNr];
+          Push[ButtonNr] = 0;
+PrintDebug();
+        }
+        else
+        {
+          // So we have a Partition here with a real choice.
+          // Change We Can Believe In means: raising any possible number,
+          //   from one-but-last back till the foremost.
+          // The last number always follows automatically to comply with MaxAtOrder.
+          // This is the first to be freed up, now that we are reconsidering.
+          for (Index=NrOfButtonAtOrder[Order]-1; Index>0; Index--)
+            if (Partition[Order][Index])  break;
+          if (Index)
+            break; /* from Ascent */
+          else  /* Index=0 */
+          {
+            // Partition of max #n has shifted all the way to [n,0, ,0,0]
+            // Bookkeeping for this abandoned Order
+            ButtonNr = ButtonAtOrder[Order][0];
+            for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+              if (Button[ButtonNr][OtherLightNr])
+                Total[OtherLightNr] -= Push[ButtonNr];
+/* Debug */
+if (1)  printf("  ^ Exhausted Partition Order #%d resetting Button=%d from Push=%d\n",
+Order, ButtonNr, Push[ButtonNr]);
+PrintDebug();
+            Push[ButtonNr] = 0;
+            // No further choices possible here, so need to backtrack further
+          }
+        } /* if real choice */
+      if (Order < 0)  break;  /* from forever */
+
+      //////////////////// Next choice ////////////////////
+
+      // As we have found a nonzero choice beyond Index=0,
+      //   there is room to increase an earlier choice.
+      Partition[Order][--Index]++;
+      ButtonNr = ButtonAtOrder[Order][Index];
+      Push[ButtonNr]++;
+      for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+        if (Button[ButtonNr][OtherLightNr])
+          Total[OtherLightNr]++;
+/* Debug */
+if (1)  printf("  + Order #%d increasing Button=%d to Push=%d\n",
+Order, ButtonNr, Push[ButtonNr]);
+PrintDebug();
+
+      // Then we reset all choices further down this Partition to zero ..
+      for (++Index; Index<NrOfButtonAtOrder[Order]; Index++)
+      {
+        Partition[Order][Index] = 0;
+        ButtonNr = ButtonAtOrder[Order][Index];
+        for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+          if (Button[ButtonNr][OtherLightNr])
+            Total[OtherLightNr] -= Push[ButtonNr];
+        Push[ButtonNr] = 0;
+      }
+
+      // .. and we figure out what the last choice should be
+      int Needed = MaxAtOrder[Order];
+      for (Index=0; Index<NrOfButtonAtOrder[Order]; Index++)
+        Needed -= Partition[Order][Index];
+      // As a nonzero choice was found someplace within this Partition,
+      //   change should definitely have been possible without breaking constraints
+      if (Needed < 0)
+      {
+        fprintf(stderr, "  + Unchangeable: Order #%d Light #%d has %d leftover at end\n",
+            Order, LightNr, Needed);
+        exit(4);
+      }
+      Partition[Order][--Index] = Needed;
+      ButtonNr = ButtonAtOrder[Order][Index];
+      Push[ButtonNr] = Needed;
+      for (OtherLightNr=0; OtherLightNr<NrOfLights; OtherLightNr++)
+        if (Button[ButtonNr][OtherLightNr])
+          Total[OtherLightNr] += Needed;
+/* Debug */
+if (1)  printf("  + Part[#%d][%d] last Button=%d set to Push=%d\n",
+Order, Index, ButtonNr, Push[ButtonNr]);
+PrintDebug();
+
+      //////////////////// Sanity Check ////////////////////
+      for (LightNr=0; LightNr<NrOfLights; LightNr++)
+        if (Total[LightNr] > Joltage[LightNr])  break;
+      if (LightNr<NrOfLights)
+      {
+/* Debug */
+if (1)  printf("  + Insanity: Order #%d Light #%d wants Joltage %d but Total at %d\n",
+Order, LightNr, Joltage[LightNr], Total[LightNr]);
+PrintDebug();
+        // This new Partition choice is not viable. Keep changing it.
+        Order++;
+        goto Ascent;
+      }
+
+    } /* forever */
+
+    Sum += MinNrOfPush;
+/* Debug */
+if (1)  printf(" Adding %d presses to get at Sum=%d..\n", MinNrOfPush, Sum);
+if (InputLineNr == 3)  break;
+  } /* while (fgets) */
+
+  /****************/
+  /* Finalisation */
+  /****************/
+  printf("After %d InputLines, found Sum %d\n", InputLineNr, Sum);
+  return 0;
+}
